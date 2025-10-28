af260b0b0e96487a41f9ea2e15cf6d8f31bfe601
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-10-28 15:52:29 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="bg-amber-400"></p>
+    </>
+  )
+}
+
+export default App
