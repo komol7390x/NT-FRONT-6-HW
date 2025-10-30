9485cd065362a21701cf740454646263ffc290d6
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-10-30 20:49:15 +0500

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import { Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <Routes>
+      <Route>
+
+      </Route>
+    </Routes>
   </>,
 )
