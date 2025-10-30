e052a62d2e4926e0e9c40b6b0ed03e97aa28cc25
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-10-30 23:31:23 +0500

    add

--- src/pages/home/components/most_read.jsx
+++ src/pages/home/components/most_read.jsx
@@ -1,7 +1,11 @@
+import girl from '../../../assets/girl_img.svg'
 export const MostRead = () => {
     return (
         <div>
-            <img src="../../../assets/girl_img.svg" alt="girl" />
+            <div className='mt-10'>
+                <p className='pl-8 text-2xl'>Кўп ўқилаётганлар</p>
+                <img src={girl} alt="girl" className='text-2xl' />
+            </div>
         </div>
     )
 }
