675ff56872fccc984738c354b11be02cd39dffe8
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-10-30 23:49:59 +0500

    add

--- src/pages/home/components/most_read.jsx
+++ src/pages/home/components/most_read.jsx
@@ -1,10 +1,15 @@
 import girl from '../../../assets/girl_img.svg'
+import { StrelkaRight, StrelkaLeft } from './strelka'
 export const MostRead = () => {
     return (
-        <div>
+        <div className='flex'>
             <div className='mt-10'>
                 <p className='pl-8 text-2xl'>Кўп ўқилаётганлар</p>
                 <img src={girl} alt="girl" className='text-2xl' />
+            </div>
+            <div className='flex'>
+                <StrelkaLeft />
+                <StrelkaRight />
             </div>
         </div>
     )
