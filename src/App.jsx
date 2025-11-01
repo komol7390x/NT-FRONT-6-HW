23ddddeb5071843da67688919e69131d2567e534
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-01 12:12:55 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { MainLayout } from './layout/main-layout'
 import { Home } from './pages/home/home'
+import { AudioBookDetail } from './pages/pruduct-detail/audio-book-detail.jsx'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<MainLayout />}>
-          <Route index element={<Home />} />
+          {/* <Route index element={<Home />} /> */}
+          <Route index element={<AudioBookDetail />} />
         </Route>
       </Routes>
     </>
