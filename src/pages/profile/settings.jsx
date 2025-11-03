38edea35593b728760fd0ac220651d3d8e024cf0
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-03 21:02:06 +0500

    add

--- src/pages/profile/settings.jsx
+++ src/pages/profile/settings.jsx
@@ -3,15 +3,26 @@ import avatar from '../../assets/png/avatar.png'
 
 export const Settings = () => {
   return (
-    <div className='border w-[855px] rounded-sm shadow-[0_4px_4px_0_rgba(0,0,0,0.06)] p-8'>
+    <div className='w-[855px] rounded-sm shadow-[0_4px_4px_0_rgba(0,0,0,0.06)] p-8'>
       <h3 className='font-bold text-[30px]'>Созламалар</h3>
       <div className='flex mt-12 items-center gap-[22px]'>
-        <div className=''>
-
+        <div className='flex items-center gap-4'>
+          <img src={avatar} alt="avatar" width={'70px'} />
+          <p className='font-semibold text-primary_color'>Сурат юклаш</p>
         </div>
-        <img src={avatar} alt="avatar" width={'70px'} />
-        <p className='font-medium text-primary_color'>Сурат юклаш</p>
       </div>
+
+      <div className='mt-8 w-[343px]' >
+        <p className='font-medium mb-3'>Исмингиз</p>
+        <input type="text"
+          placeholder='Исмингиз'
+          className='border border-[#EDEDED] py-3 w-full px-3 rounded-2xl'
+        />
+        <div className='mt-[40px] w-full py-3 bg-primary_color rounded-2xl'>
+          <p className='text-center text-white'>Сақлаш</p>
+        </div>
+      </div>
+
     </div>
   )
 }
