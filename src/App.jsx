15f8432235c0c0f2fd9b994f137c4e0c455f2eca
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-07 20:56:52 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React from "react"
-import { AppContext } from "./context/app-context/app-context"
-import { Form } from "./components/form";
-import { Card } from "./components/card";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const { data } = React.useContext(AppContext);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="container">
-      <Form />
-      {data?.userList?.map((item) => (
-        <Card username={item.username} key={item.id} id={item.id} />
-      ))}
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AppContextProvider } from './context/app-context/app-context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <AppContextProvider>
+  <StrictMode>
     <App />
-  </AppContextProvider>,
+  </StrictMode>,
 )
