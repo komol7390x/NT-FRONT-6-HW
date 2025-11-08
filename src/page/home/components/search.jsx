0aa9c6bb7295417c3339710b6deec6d3d05ab787
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-08 11:24:19 +0500

    add

--- src/page/home/components/search.jsx
+++ src/page/home/components/search.jsx
@@ -3,7 +3,7 @@ export const SearchIcon = () => {
         <div className="border">
             <input type="text" className="rounded-[100px] bg-white
              text-[#555] py-5 shadow-[0_4px_10px_rgba(0,0,0,0.15)]
-            text-[16px] font-medium pl-5 w-full max-w-[600px] flex mx-auto" placeholder="Find the car of your dreams" />
+            text-[16px] font-medium pl-5 w-full max-w-[600px] flex mx-auto overflow-hidden focus:outline-none" placeholder="Find the car of your dreams" />
         </div>
     )
 }
