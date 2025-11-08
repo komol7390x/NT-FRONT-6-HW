e388be239101b005d01d4af26c2208b6d4012901
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-08 15:15:05 +0500

    add

--- src/page/home/components/search.jsx
+++ src/page/home/components/search.jsx
@@ -8,7 +8,7 @@ export const Search = () => {
             <div className="flex relative max-w-[600px] mx-auto">
                 <input type="text" className="rounded-[100px] bg-white
              text-[#555] py-5 shadow-[0_4px_10px_rgba(0,0,0,0.15)]
-            text-[16px] font-medium pl-5 w-full max-w-[600px] flex mx-auto overflow-hidden focus:outline-none" placeholder="Find the car of your dreams" />
+            text-[16px] font-medium pl-5 w-full max-w-[600px] pr-17 flex mx-auto overflow-hidden focus:outline-none" placeholder="Find the car of your dreams" />
                 <div className="bg-primary-color inline absolute right-0 rounded-full p-4 mr-1 mt-1">
                     <SearchIcon />
                 </div>
