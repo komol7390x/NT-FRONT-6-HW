f8b7d85d084930a387fddf8fcac8e2733a6b4aec
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-10 23:53:59 +0500

    a

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,7 @@
 export const Profile = () => {
   return (
     <div className="container">
-      <div className="h-screen bg-yellow-300 uppercase flex items-center justify-center">Profile</div>
+      <div className="h-screen bg-yellow-300 uppercase flex items-center justify-center">Profile1</div>
     </div>
   )
 }
