67b2730ffc1ffcd67f4561c19ef6df2aca31d085
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-15 19:48:34 +0500

    add

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import notFound from '../assets/gif/404.gif'
 
 export const NotFound = () => {
     return (
-        <div className='container h-screen'>404 not found pages</div>
+        <div className='container'>
+            <div className='w-full mx-auto flex justify-center items-center'>
+                <img src={notFound} alt="404" className="w-[900px] h-[500px]" />
+            </div>
+        </div>
     )
 }
